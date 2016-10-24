b0d6f766c2ce6d13557be343743b0e128c5b32f0
Viktor Krashininnikov <krasherspost@gmail.com>
2016-10-24 14:07:36 +0000

    Improve GetResponse class

--- tcounter.py
+++ tcounter.py
@@ -2,26 +2,30 @@ import pycurl
 from io import BytesIO
 
 class GetResponse:
-  buffer = BytesIO()
+  """
+  This class help you to get source code of any Web-page
+     Use:
+       GetResponse.encoding(value) - to change encoding for decoding HTML page
+       GetResponse.get() - to get source code of Web-page
+  """
   def __init__(self, value):
     self.url = value
-    print(self.url)
-  def setbuf(self, value):
-    self.buffer = value
-    print(self.buffer)
-  def request(self):
+    self.buffer = BytesIO()
+    self.enc = 'utf-8'
+    print("Inspected URL: {}".format(self.url))
+  def encoding(self, value):
+    self.enc = value
+    print("Encoding has been changed to {}".format(self.enc))
+  def get(self):
     c = pycurl.Curl()
     c.setopt(c.URL, self.url)
     c.setopt(c.WRITEDATA, self.buffer)
     c.perform()
     c.close()
-    print(c)
-  def get(self):
-    self.body = self.buffer.getvalue()
-    # Body is a string in some encoding.
-    # In Python 2, we can print it without knowing what the encoding is.
-    print(self.body.decode('iso-8859-1'))
+    body = self.buffer.getvalue()
+    print(body.decode(self.enc))
 
-body = GetResponse('http://pycurl.io/')
-body.request()
-body.get()
+url = input("Please specify URL for inspecting [pycurl.io]: ")
+url = url or 'pycurl.io'
+response = GetResponse(url)
+body = response.get()
